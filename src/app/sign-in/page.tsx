fdb08d3e440f2e61315a3b4604fb067350cba7aa
marijae04 <melezovic4@gmail.com>
2023-09-10 20:30:28 +0200

    sign in page

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,7 +1,35 @@
-export default function SignIn(){
+
+async function login(data: FormData){
+    "use server"
+
+    console.log("Login")
+}
+
+async function register(data: FormData){
+    "use server"
+
+    console.log("Register")
+}
+
+export default function SignInPage(){
     return (
         <div>
-            <h1>Sign in</h1>
+            <div className="log-in-container">
+                <h1>Log in</h1>
+                <form action="login">
+                    <input type="text" name="username" placeholder="Username" />
+                    <input type="password" name="password" placeholder="Password" />
+                </form>
+            </div>
+            <div className="register-container">
+                <h1>Register</h1>
+                <form action="register">
+                    <input type="text" name="name" placeholder="Name" />
+                    <input type="email" name="email" placeholder="email@gmail.com" />
+                    <input type="text" name="username" placeholder="Username" />
+                    <input type="password" name="password" placeholder="Password" />
+                </form>
+            </div>
         </div>
     )
 }
