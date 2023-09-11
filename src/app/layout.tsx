b101233217086cdbabda34a5d7bd8ee233099e27
marijae04 <melezovic4@gmail.com>
2023-09-11 06:43:35 +0200

    basic tailwindcss for login and register form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,17 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export const metadata: Metadata = {
-  title: 'Kadmo app',
+  title: 'Kadmo',
   description: 'Meet new cultures with Kadmo',
 }
+
+const font = Montserrat ({ 
+  subsets: ['latin'], 
+});
 
 export default function RootLayout({
   children,
@@ -16,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={font.className}>{children}</body>
     </html>
   )
 }
--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -13,24 +13,81 @@ async function register(data: FormData){
 
 export default function SignInPage(){
     return (
-        <div>
-            <div className="log-in-container">
-                <h1>Log in</h1>
-                <form action="login">
-                    <input type="text" name="username" placeholder="Username" />
-                    <input type="password" name="password" placeholder="Password" />
-                    <button type="submit">Login</button>
-                </form>
-            </div>
-            <div className="register-container">
-                <h1>Register</h1>
-                <form action="register">
-                    <input type="text" name="name" placeholder="Name" />
-                    <input type="email" name="email" placeholder="email@gmail.com" />
-                    <input type="text" name="username" placeholder="Username" />
-                    <input type="password" name="password" placeholder="Password" />
-                    <button type="submit">Register</button>
-                </form>
+        
+        <div className="relative h-full w-full bg-[url('/images/wallpaper.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+            <div className="bg-black w-full h-full bg-opacity-70">
+
+                <nav className="px-10 py-3 absolute top-0 left-0">
+                    <img src="/images/logo.png" className="h-12" alt="Logo" />
+                </nav>
+
+                <div className="min-h-screen flex flex-col sm:flex-row justify-center items-center">
+                    <div className="text-white text-center p-12 rounded-lg flex-1 order-2 mt-5 sm:order-1">
+                        <div className="text-4xl mb-4 font-semibold">Log in</div>
+                        <form action="login" className="mt-3">
+                        <div className="mb-4">
+                            <input
+                            type="text"
+                            name="username"
+                            placeholder="Username"
+                            className="input-field w-4/5 text-black rounded-[50px] text-center"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <input
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                            className="input-field w-4/5 text-black rounded-[50px] text-center"
+                            />
+                        </div>
+                        <button type="submit" className="btn-primary bg-green-700 hover:bg-green-900 tranisitio w-4/5  rounded-[50px]">
+                            Login
+                        </button>
+                        </form>
+                    </div>
+
+                    <div className="ml-6 text-white text-center p-9 rounded-lg flex-1 order-1 mt-10 sm:order-2">
+                        <div className="text-4xl mb-4 font-semibold">Register</div>
+                        <form action="register" className="mt-4">
+                        <div className="mb-4">
+                            <input
+                            type="text"
+                            name="name"
+                            placeholder="Name"
+                            className="input-field w-4/5 text-black rounded-[50px] text-center"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <input
+                            type="email"
+                            name="email"
+                            placeholder="email@gmail.com"
+                            className="input-field w-4/5 text-black rounded-[50px] text-center"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <input
+                            type="text"
+                            name="username"
+                            placeholder="Username"
+                            className="input-field w-4/5 text-black rounded-[50px] text-center"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <input
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                            className="input-field w-4/5 text-black rounded-[50px] text-center"
+                            />
+                        </div>
+                        <button type="submit" className="btn-primary bg-green-700 hover:bg-green-900 tranisitio w-4/5 rounded-[50px]">
+                            Register
+                        </button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     )
