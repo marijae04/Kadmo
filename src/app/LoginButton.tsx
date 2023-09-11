02842a303c40fa1ffeced5befd2b9f1c6d007b7b
marijae04 <melezovic4@gmail.com>
2023-09-11 14:13:15 +0200

    minor fix

--- src/app/LoginButton.tsx
+++ src/app/LoginButton.tsx
@@ -8,9 +8,7 @@ const LoginButton = () => {
     <div className="ml-auto flex gap-2">
       {session?.user ? (
         <>
-          <p className="text-sky-600Bane
-
-￼Sign Out"> {session.user.name}</p>
+          <p className="text-sky-600"> {session.user.name}</p>
           <button className="text-red-500" onClick={() => signOut()}>
             Sign Out
           </button>
