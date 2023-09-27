c2afb39fdb58516184857f7ddc7b1f5d1c3fb22b
marijae04 <melezovic4@gmail.com>
2023-09-27 15:09:01 +0200

    favicon

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-        serverActions: true,
-        appDir: true
+        serverActions: true
     }
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
